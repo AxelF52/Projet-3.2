b548f8ecdae8346f836f07beb282d25f65683c5d
CamomilleV <camille.sehy@gmail.com>
2024-11-25 10:39:35 +0100

    test

--- jeu.py
+++ jeu.py
@@ -11,4 +11,28 @@ def draw():
 def update():
    pass
 
+class Player:
+    """
+    Joueur : collisions et mouvements
+    """
+    def __init__(self, x, y):
+        global tps
+        self.x = x
+        self.y = y
+        self.cam_x = 0
+        self.cam_y = 0
+        self.speed = 2
+        self.size = 8
+        self.direction = 1
+
+    def move(self, dx, dy):
+         if not self.is_collision(self.x + dx, self.y + dy):
+            if dx < 0:
+               self.direction = -1
+            elif dx > 0:
+               self.direction = 1
+            self.x += dx
+            self.y += dy
+
+
 pyxel.run(update, draw)
