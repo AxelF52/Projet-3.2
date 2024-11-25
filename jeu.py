39ff14f936fe969578472c6e210eee7687cbbaea
CamomilleV <camille.sehy@gmail.com>
2024-11-25 10:36:45 +0100

    Base du jeu

--- jeu.py
+++ jeu.py
@@ -3,3 +3,12 @@ import pyxel
 pyxel.init(256, 256, title="Projet 3.2")
 pyxel.fullscreen(False)
 pyxel.load("data.pyxres")
+
+
+def draw():
+   pyxel.cls(0)
+
+def update():
+   pass
+
+pyxel.run(update, draw)
